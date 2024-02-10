817e06ac6a11a1a101af353980e439881a02d3fb
jayshree748 <159608797+jayshree748@users.noreply.github.com>
2024-02-10 23:15:06 +0530

    Add files via upload

---
+++ Print10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x , y;
+    cout << "Enter the number less than 6" << endl;
+    cin >> x >> y;
+    if((x + y) == 10)
+        cout << "10" << endl;
+    else
+        cout << "Not fullfill Requirement" << endl;
+
+}
